17be696d7c5d7325d113132b0049dea4d7c95299
SARAH <HALSEYSARAH@Gmail.com>
2014-08-12 12:34:37 -0400

    Exercise 2 added!

---
+++ exercise2.rb
@@ -0,0 +1,25 @@
+#Adding tip to 55 dollar meal 
+tip_amount = 55 * 0.20
+puts "Your meal was $55, the suggested tip is $#{tip_amount.to_i}"
+
+total_amount = 55 * 0.20 + 55 
+puts "Your total bill is $#{total_amount.to_i}"
+
+##How can I add {tip_amount} + 55 easier?
+
+#Converting integers to strings
+puts "Your height is " + 185.to_s + "cm"
+
+#Result of 45628 x 7839 with interpolation 
+x = 45628 
+y = 7839
+puts "The value of x is #{x}."
+puts "The value of y is #{y}."
+puts "The value of x times y is #{x * y}"
+
+#Booleans
+puts (true && false) || (false && true) || !(false && false)
+
+
+
+
