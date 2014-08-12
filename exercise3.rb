10b9f41a3edb29fe21c09159d3bddb428425fd47
SARAH <HALSEYSARAH@Gmail.com>
2014-08-12 14:03:55 -0400

    Exercise 3

---
+++ exercise3.rb
@@ -0,0 +1,22 @@
+puts "What is your name?"
+name = gets.chomp
+puts "Hi #{name}!"
+
+
+my_name = 'Sarah'
+puts "Nice to meet you, my name is #{my_name}!"
+
+puts "How old are you?"
+age = gets.chomp
+puts "You are #{age} years old!"
+
+my_age = 23
+puts "Cool! What about me?"
+puts "Oh! I am #{my_age} :)"
+
+
+
+
+
+
+
