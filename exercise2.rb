5394db2e7e268c695b5e971ba4bd8420a9cfe9dd
SARAH <HALSEYSARAH@Gmail.com>
2014-08-12 15:45:33 -0400

    Updated Exercise 2

--- exercise2.rb
+++ exercise2.rb
@@ -1,11 +1,11 @@
 #Adding tip to 55 dollar meal 
+meal_cost = 55
 tip_amount = 55 * 0.20
 puts "Your meal was $55, the suggested tip is $#{tip_amount.to_i}"
 
-total_amount = 55 * 0.20 + 55 
+total_amount = tip_amount + meal_cost
 puts "Your total bill is $#{total_amount.to_i}"
 
-##How can I add {tip_amount} + 55 easier?
 
 #Converting integers to strings
 puts "Your height is " + 185.to_s + "cm"
